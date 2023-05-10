e37855f24d13cb19d14310fd2c24a7d2bb7d1100
Agosh Saini <33809549+agosh-saini@users.noreply.github.com>
2023-05-10 00:53:32 -0400

    creating modules

---
+++ config.py
@@ -0,0 +1,3 @@
+# added to .gitignore
+
+openai_api_key = "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"
--- gpt_ranking.py
+++ gpt_ranking.py
@@ -1,5 +1,26 @@
 from patent_db import gen_db
 import pandas as pd
+import openai
+import config
+
+# Define a function to compute the text similarity between two strings using GPT-3
+def compute_similarity(query, text):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Similarity between '{query}' and '{text}':",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    return float(response.choices[0].text.strip())
+
+
+# Initialize the OpenAI API client
+openai.api_key = config.openai_api_key
+
+# Define your search query
+search_query = "self driving"
 
 # creating patent workspace based on results
 
@@ -7,4 +28,13 @@ path = "C:\\Users\\westw\\Documents\\Project\\patent_search_helper\\gp-search-te
 
 df = gen_db(path)
 
-print(df)
+# Compute the similarity scores between the search query and each row in the dataframe
+similarity_scores = df["Claims"].apply(lambda x: compute_similarity(search_query, x))
+
+# Add the similarity scores to the dataframe
+df["similarity_score"] = similarity_scores
+
+# Sort the dataframe by similarity score in descending order
+df_sorted = df.sort_values(by="similarity_score", ascending=False)
+
+df_sorted.to_csv("sorted results", index=False)
